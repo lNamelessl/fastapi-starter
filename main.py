c6405e70f8673a5a546a8fef000805cad3f608fc
lNameless <ikasima0@gmail.com>
2026-05-14 15:15:08 +0100

    chore: add health check

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 
 @app.get("/")
 async def root():
     return {"Hello": "World!"}
+
+
